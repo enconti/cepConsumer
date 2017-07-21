455bacbdaded456717d564eeba5fc40d2c6205dd
Emerson Contiero <emerson.contiero@neogrid.com>
2017-07-21 17:07:29 -0300

    Inicitalize

---
+++ src/main/java/com/enconti/cepConsumer/CepConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.enconti.cepConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepConsumerApplication.class, args);
+	}
+}
